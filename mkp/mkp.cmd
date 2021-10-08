0152f001d032017d3290842a3cb0c3bc8aea5bd3
Evgeni V <58845427+RE-Specto@users.noreply.github.com>
2021-10-08 18:20:16 +0300

    first version

---
+++ mkp/mkp.cmd
@@ -0,0 +1,44 @@
+@echo off
+cd ^"%~dp0^"
+
+if [%1] == [] GOTO about
+set exename=^"%~nx1^"
+set folder=^"%~dp1*^"
+set output=^"%~dpn1-sfx.exe^"
+
+echo creating config file
+echo.
+echo ;!@Install@!UTF-8! >config.txt
+echo RunProgram=%exename% >>config.txt
+echo ;!@InstallEnd@! >>config.txt
+
+del archive.7z 2>nul
+echo compressing
+echo.
+7zr a archive.7z %folder% -mx -mf=BCJ2
+
+echo creating sfx
+copy /b 7zS2.sfx + config.txt + archive.7z %output%
+
+del archive.7z>nul
+del config.txt>nul
+set exename=
+set folder=
+set output=
+
+GOTO end
+
+:about
+echo a script to create a portable app from your files
+echo including all prerequesites from the same directory
+echo.
+echo usage: mkp YourExecutableFile.exe
+echo.
+echo also you can drag and drop a file here, and press enter
+echo or just press enter to exit
+set /p file=
+if [%file%] == [] exit
+if exist %file% (%0 %file%) else (%0)
+exit
+
+:end
---
+++ mkp_install.cmd
@@ -0,0 +1,3 @@
+@copy /y "%~dp0mkp.exe" "%APPDATA%\Microsoft\Windows\SendTo\make portable.exe"
+@copy /y "%~dp0mkpn.exe" "%APPDATA%\Microsoft\Windows\SendTo\make portable (no admin).exe"
+@timeout /t 2 >nul
